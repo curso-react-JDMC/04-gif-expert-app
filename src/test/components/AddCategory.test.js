b254066670876c044b52b5eb095986b7305e09c2
Juan David Mendez Campuzano <juandavidmendezcampuzano@gmail.com>
2022-01-15 21:03:52 -0500

    cambios en addCategoy.test

--- src/test/components/AddCategory.test.js
+++ src/test/components/AddCategory.test.js
@@ -1,11 +1,13 @@
 import { AddCategory } from './../../components/AddCategory.js';
 import React from 'react';
-const {shallow} = require("enzyme");
+const { shallow } = require("enzyme");
 
 describe('Pruebas en AddCategory', () => {
-    test('debe mostrarse correctamente',()=>{
-        const setCategories = ()=>{};
-        const wrapper = shallow(<AddCategory setCategories={ setCategories }/>); 
-        expect(wrapper).toMatchSnapshot();
-    })
+  const setCategories = () => { };
+  const wrapper = shallow(<AddCategory setCategories={setCategories} />);
+  test('debe mostrarse correctamente', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+  test('', () => { })
 })
+
